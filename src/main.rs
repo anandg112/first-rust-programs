c45717fe28caa2e2260dd75b5d444058ab98b1e2
anandg112 <anandg112@gmail.com>
2021-03-20 15:01:24 -0400

    first rust program

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut arguments = std::env::args().skip(1);
+    let key = arguments.next().unwrap();
+    let value = arguments.next().unwrap();
+    println!("The key is {} and the value is {}", key, value);
+    let contents = format!("{}\t{}\n", key, value);
+    std::fs::write("kv.db", contents).unwrap();
+
+    let database = Database::new().expect("Database::new() crashed");
+}
+
+struct Database {
+    map: HashMap<String, String>,
+}
+
+impl Database {
+    fn new() -> Result<Database, std::io::Error> {
+        // populate the map
+        // read the kv.db file
+        let mut map = HashMap::new();
+        let contents = std::fs::read_to_string("kv.db")?;
+        // parse the string
+        for line in contents.lines() {
+            let mut chunks = line.splitn(2, '\t');
+            let key = chunks.next().expect("No key!");
+            let value = chunks.next().expect("No value");
+            map.insert(key.to_owned(), value.to_owned());
+        }
+        Ok(Database { map: map })
+    }
+}
